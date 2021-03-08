8b0ad9155ecfaa30e4e4fba713eb09fd5f425247
K.Anais <kiamanais@gmail.com>
2021-03-08 02:13:24 +0100

    home page layout and scripting

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
-$( function() {
-    $( "input" ).checkboxradio();
-  } );
+function Order(size,crust,toppings,number,deliver){
+    this.size = size;
+    this.crust = crust;
+    this.toppings = toppings;
+    this.number = number;
+    this.deliver = deliver;
+}
+
+function getPrice(){
+    
+}
